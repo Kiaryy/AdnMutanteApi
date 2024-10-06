cd2cafc706a9740949cde32ee1175abee45a62dc
Kiaryy <martin360.moron@gmail.com>
2024-10-06 17:34:40 -0300

    Added Unit Testing

--- src/test/java/com/example/AdnMutanteApi/AdnMutanteApiApplicationTests.java
+++ src/test/java/com/example/AdnMutanteApi/AdnMutanteApiApplicationTests.java
@@ -1,13 +1,53 @@
 package com.example.AdnMutanteApi;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import static com.example.AdnMutanteApi.logic.dnaCheck.isMutant;
+import static com.example.AdnMutanteApi.logic.ArrayToMatrix.turnArrayToMatrix;
+import com.example.AdnMutanteApi.Exceptions.DnaFormatException;
 
 @SpringBootTest
 class AdnMutanteApiApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void horizontalMutant() {
+		String[] dna = {"CCCC", "CATG", "TGCA","GGCA"};
+		try {
+			boolean isMutant = isMutant(turnArrayToMatrix(dna));
+			Assertions.assertEquals(true, isMutant);
+		} catch (DnaFormatException e) {
+			System.out.println(e.getMessage());
+		}
 	}
-
+	@Test
+	void verticalMutant(){
+		String[] dna = {"ATGC", "GTCA", "CTAA","GTCG"};
+		try {
+			boolean isMutant = isMutant(turnArrayToMatrix(dna));
+			Assertions.assertEquals(true, isMutant);
+		} catch (DnaFormatException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	@Test
+	void leftRightDiagonalMutant(){
+		String[] dna = {"AGGC", "GACA", "CTAC","CTGA"};
+		try {
+			boolean isMutant = isMutant(turnArrayToMatrix(dna));
+			Assertions.assertEquals(true, isMutant);
+		} catch (DnaFormatException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+	@Test
+	void rightLeftDiagonalMutant(){
+		String[] dna = {"AGGC", "GACA", "CCGC","CTGA"};
+		try {
+			boolean isMutant = isMutant(turnArrayToMatrix(dna));
+			Assertions.assertEquals(true, isMutant);
+		} catch (DnaFormatException e) {
+			System.out.println(e.getMessage());
+		}
+	}
 }
